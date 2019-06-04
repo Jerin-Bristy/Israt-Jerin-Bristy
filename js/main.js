80df30c5aaf2ada90c13dc1f903024983e5040f2
Jerin-Bristy <ijerin1112@gmail.com>
2019-06-04 12:19:19 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var modified = document.querySelector('.modified');
+var loc = document.querySelector('.location');
+modified.textContent = document.lastModified;
+loc.textContent = window.location.href;
+
+
